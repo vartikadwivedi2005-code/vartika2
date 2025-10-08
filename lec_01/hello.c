39dd4c436d80b5c750679eed870eb7d373e6072e
vartikadwivedi2005-code <vartikadwivedi2005@gmail.com>
2025-10-08 16:03:59 +0530

    first commit

---
+++ lec_01/hello.c
@@ -0,0 +1,16 @@
+#include <stdio.h>   // Header file (standard input-output)
+
+int main() {
+    // Variable create karna
+    int age = 20;          // integer variable (sirf numbers store karega)
+    float pi = 3.14;       // decimal value ke liye
+    char grade = 'A';      // ek single character ke liye
+
+    // Print karna
+    printf("Hello Students!\n");     // simple text print
+    printf("Age = %d\n", age);       // %d integer ke liye
+    printf("Pi = %f\n", pi);         // %f float ke liye
+    printf("Grade = %c\n", grade);   // %c character ke liye
+
+    return 0;
+}
